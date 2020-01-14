188be998bf2bf14ca2d745f1acbd0a45a9f4070e
Mev <36679013+Hirss@users.noreply.github.com>
2020-01-14 14:05:13 +0100

    Update

--- Montage12.ino
+++ Montage12.ino
@@ -1,21 +1,25 @@
+#define S1 8
 #define vert 13
 #define orange 12
-#define rouge 11
+#define rouge 7
 
 void setup() {
-  pinMode(verte, OUTPUT);
+  pinMode(S1, INPUT);
+  pinMode(vert, OUTPUT);
   pinMode(orange, OUTPUT);
   pinMode(rouge, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(rouge, HIGH);
+  if(digitalRead(S1) == true) {
+    digitalWrite(rouge, HIGH);
   delay(7000);
-  digitalWrite(rouge, LOW);
-  digitalWrite(orange, HIGH);
+    digitalWrite(rouge, LOW);
+    digitalWrite(orange, HIGH);
   delay(1000);
-  digitalWrite(orange, LOW);
-  digitalWrite(verte, HIGH);
+    digitalWrite(orange, LOW);
+    digitalWrite(vert, HIGH);
   delay(6000);
-  digitalWrite(verte, LOW);
+    digitalWrite(vert, LOW);
+  }
 }
