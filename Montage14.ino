12542364a5eabc1bcae1c1428ff266160a5015ad
Mev <36679013+Hirss@users.noreply.github.com>
2020-01-20 10:13:55 +0100

    Update Montage14.ino

--- Montage14.ino
+++ Montage14.ino
@@ -1,39 +1,56 @@
-#define S1 8
-#define S2 12
-#define vert 13
-#define rouge 7
-
-int count;
 
 
-void setup() {
-  pinMode(S1, INPUT);
-  pinMode(S2, INPUT);
-  pinMode(vert, OUTPUT);
-  pinMode(rouge, OUTPUT);
-  Serial.begin(9600);
-}
+    #define S1 2            //le port 2 prend le nom S1
+    #define S2 3            //le port 2 prend le nom S1
+    #define H1 12
+    #define H2 13           //le port 12 prend le nom H1
+    bool etatS1;            //déclaration d'une variable
+    bool etatS2;            //déclaration d'une variable
+    int compteur;
+    bool etatS1_apres;
+    bool etatS2_apres;
+     
+    void setup()
+    {
+    pinMode(S1, INPUT);     //S1 est une entrée
+    pinMode(S2, INPUT);     //S1 est une entrée
+    pinMode(H1, OUTPUT);        //H est une sortie
+    pinMode(H2, OUTPUT);
+    Serial.begin(9600);
+     
+    }
+     
+    void loop()
+    {
+      etatS1=digitalRead(S1);  
+      etatS2=digitalRead(S2);
+     
+    if (etatS1==true&&etatS1_apres!=etatS1)
+    compteur++;
+       etatS1_apres=etatS1;
+     
+    if (compteur>=5)       
+    {
+      digitalWrite(H2,HIGH);
+    }
+     
+        if (etatS2==true&&etatS2_apres!=etatS2)
+          compteur--;
+       etatS2_apres=etatS2;
+     
+      if (compteur<=5)     
+       digitalWrite(H2,LOW);
+      digitalWrite(H1,HIGH);
+     
+      if (compteur==5)
+    {
+    digitalWrite(H2,HIGH);
+        digitalWrite(H1,LOW);
+    }
+     
+       
+     
+      Serial.print(" le compteur= ") ;
+    Serial.println(compteur);
+      }
 
-void loop() {
-  if(digitalRead(S1) == true && count < 5) {
-    digitalWrite(vert, HIGH);
-    count++;
-  }
-  
-  if(count >= 5){
-    digitalWrite(vert, LOW);
-    digitalWrite(rouge, HIGH);
-  }
-  
-  if(count < 5) {
-    digitalWrite(rouge, LOW);
-    digitalWrite(vert, HIGH);
-  }
-  
-  if(digitalRead(S2) == true) {
-    count--;
-  } 
-  
-  Serial.print("Places = ");
-  Serial.println(count);
-}
